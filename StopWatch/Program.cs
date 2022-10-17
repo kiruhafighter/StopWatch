10ab3faed0ea4e6566db8c613e6ea8c356eb8465
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2022-10-17 16:50:13 +0300

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            do
+            {
+                Console.WriteLine("Choose go or stop (1 OR 2)");
+                ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        {
+                            DateTime start = new DateTime();
+                        C1:
+                            Console.WriteLine("Type start :");
+                            string startStr = Console.ReadLine();
+                            if (startStr == "start" || startStr == "Start")
+                            {
+                                start = DateTime.Now;
+                                start.ToLongTimeString();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid command , try again");
+                                goto C1;
+                            }
+                        C2:
+                            Console.WriteLine("Type stop : ");
+                            string stopStr = Console.ReadLine();
+                            if (stopStr == "Stop" || stopStr == "stop")
+                            {
+                                TimeSpan result = StopWatcher.Stop(start);
+                                Console.WriteLine("Time Span is :");
+                                Console.WriteLine(result.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid command , try again");
+                                goto C2;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
+
+            } while (ch != 2);
+                
+            
+        }
+    }
+}
---
+++ StopWatch/StopWatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class StopWatcher
+    {
+        
+        public static TimeSpan Stop(DateTime start)
+        {
+            DateTime stopTime = DateTime.Now;
+            stopTime.ToLongTimeString();
+            TimeSpan timer =  stopTime - start;
+            
+            return timer;
+        }
+
+
+    }
+}
